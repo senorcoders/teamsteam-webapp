b6acfc9930e2cb9f52e365fbe12a65eed04b765c
bdangla-senorcoders <brian@senorcoders.com>
2018-07-10 17:29:45 -0600

    View Roster for teams

--- src/app/core/menu/menu-items/menu-items.ts
+++ src/app/core/menu/menu-items/menu-items.ts
@@ -35,7 +35,7 @@ const MENUITEMS = [
     state: 'player',
     name: 'Players',
     type: 'sub',
-    icon: 'fa fa-user',
+    icon: 'icon-people icons',
     children: [
       {state: 'add-player', name: 'Add Player'},
       {state: 'list-player', name: 'List Player'},
