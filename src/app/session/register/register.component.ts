9e53f7f2d7a8af17a0bd3acc9fe6ea7b90fb22f7
bdangla-senorcoders <brian@senorcoders.com>
2018-07-11 16:31:17 -0600

    fix register page

--- src/app/session/register/register.component.ts
+++ src/app/session/register/register.component.ts
@@ -15,36 +15,99 @@ import { ToastrService } from 'ngx-toastr';
 export class RegisterComponent {
   sports: Array<any>;
 	registerForm:FormGroup;
+  uuid:any;
+  model='Navigator';
+  platform:any;
+  versionOS:any;
+  showEmailStatus:boolean=false;
+  showButton:boolean=true
   constructor(
-    private teamService:TeamService, private fb:FormBuilder,private toastr: ToastrService, private auth:AuthenticationService
+    private teamService:TeamService, private fb:FormBuilder,private toastr: ToastrService, private auth:AuthenticationService, private router:Router
   ) { }
   ngOnInit() {
+    this.getSO();
+    this.getBrowserVersion();
+    this.randomString();
     this.sports=[
 		{
 	    id: 'default',
 	    text: 'choose one'
 		},
 		{
-			id:'soccer',
-			text:'soccer'
-		},
-		{
 			id:'football',
-			text:'football'
+			text:'SOCCER'
 		},
 		{
-			id:'voleyball',
-			text:'voleyball'
+			id:'basketball',
+			text:'BASKETBALL'
+		},
+		{
+			id:'bowling',
+			text:'BOWLING'
 		},
 		{
 			id:'baseball',
-			text:'baseball'
-		}
+			text:'BASEBALL & SOFTBALL'
+		},
+    {
+      id:'cheerleading',
+      text:'CHEERLEADING'
+    },
+    {
+      id:'crew&rowing',
+      text:'CREW&ROWING'
+    },
+    {
+      id:'crosscountry',
+      text:'BCROSSCOUNTRY'
+    },
+    {
+      id:'fieldhockey',
+      text:'FIELDHOCKEY'
+    },
+    {
+      id:'golf',
+      text:'GOLF'
+    },
+    {
+      id:'hockey',
+      text:'HOCKEY'
+    },
+    {
+      id:'lacrosse',
+      text:'LACROSSE'
+    },
+    {
+      id:'rugby',
+      text:'RUGBY'
+    },
+    {
+      id:'swimming&diving',
+      text:'SWIMMING&DIVING'
+    },
+    {
+      id:'tennis',
+      text:'TENNIS'
+    },
+    {
+      id:'track&field',
+      text:'TRACK&FIELD'
+    },
+    {
+      id:'volleyball',
+      text:'VOLLEYBALL'
+    },
+    {
+      id:'waterpolo',
+      text:'WATERPOLO'
+    },
+    {
+      id:'WRESTLING',
+      text:'WRESTLING'
+    }
 		];
     this.registerForm=this.fb.group({
   		username:['', Validators.required],
-  		password:['', Validators.required],
-  		repassword:['', Validators.required],
   		firstName:['', Validators.required],
   		lastName:['', Validators.required],
   		email:['', [Validators.required, Validators.email]],
@@ -53,62 +116,84 @@ export class RegisterComponent {
   		sport:['', Validators.required],
   		city:['', Validators.required]
   	})
-  	}
+  }
   register(){
-  	//check password and repassword
-  	if (this.registerForm.get('password').value==this.registerForm.get('repassword').value) {
-  		//prepare data
-  		let data={
-  			"username":this.registerForm.get('username').value,
-  			"password":this.registerForm.get('password').value,
-  			"firstName":this.registerForm.get('firstName').value,
-  			"lastName":this.registerForm.get('lastName').value,
-  			"email":this.registerForm.get('email').value,
-  			"teamName":this.registerForm.get('teamName').value,
-  			"description":this.registerForm.get('description').value,
-  			"sport":this.registerForm.get('sport').value,
-  			"city":this.registerForm.get('city').value,
-  			"configuration": { "valid": true }
-  		}
-  		this.auth.checkEmail(this.registerForm.get('email').value).subscribe(
-	  		result=>{
-	  			if (result['valid']) {
-	  				this.teamService.register(data).subscribe(
-			  			result=>{
-			  				this.showSuccess('Team Registered');
-                let loginCredential={
-                  'email':data.email,
-                  'password':data.password
-                }
-                this.auth.login(loginCredential).subscribe(
-                  result=>{
-                    this.auth.setLoginData(result)
-                    window.location.href = '/dashboard/dashboard-v1';
-                  },
-                  error=>{
-                    this.showError(error.error)
-                    console.log(error)
-                  }
-                )
-			  			},
-			  			error=>{
-			  				this.showError(error.error);
-			  				console.log(error);
-			  			}
-					)
-	  			}
-			  	else{
-			  		this.showError('Email is already in use')
-			  	}
-	  		},
-	  		error=>{
+    prepare data
+  	let data={
+			"username":this.registerForm.get('username').value,
+			"firstName":this.registerForm.get('firstName').value,
+			"lastName":this.registerForm.get('lastName').value,
+			"email":this.registerForm.get('email').value,
+      "uuid": this.uuid ,
+      "model": this.model,
+      "platform": this.platform,
+      "versionOS": this.versionOS,
+			"teamName":this.registerForm.get('teamName').value,
+			"description":this.registerForm.get('description').value,
+			"sport":this.registerForm.get('sport').value,
+			"city":this.registerForm.get('city').value,
+      "newTeam": true,
+			"configuration": { "valid": true }
+  	}
+		this.teamService.register(data).subscribe(
+			result=>{
+				this.showSuccess('Team Registered, Login to enjoy the app');
+        this.registerForm.reset();
+        this.router.navigate(['/loginone'])
+			},
+			error=>{
+				this.showError(error.error);
 				console.log(error);
-	  		}
-  		)
-  	}
-  	else{
-  		this.showError('Password and Repeat password not matched')
-  	}
+			}
+	  )
+  }
+  checkEmail(){
+    this.auth.checkEmail(this.registerForm.get('email').value).subscribe(
+      result=>{
+        if(result['valid']){
+          this.showEmailStatus=false
+          this.showButton=true
+        }
+        else{
+          this.showEmailStatus=true;
+          this.showButton=false;
+        }
+      }
+    )
+  }
+  //get SO
+  getSO(){
+    this.platform=window.navigator.appVersion
+  }
+  //generate uuid random
+  randomString() {
+    let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+    let string_length = 7;
+    let randomstring = '';
+    for (let i=0; i<string_length; i++) {
+      let rnum = Math.floor(Math.random() * chars.length);
+      randomstring += chars.substring(rnum,rnum+1);
+    }
+    this.uuid=randomstring;
+  }
+  //get browser version
+  getBrowserVersion(){
+    let sayswho= (()=>{
+      var ua= navigator.userAgent, tem, 
+      M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+      if(/trident/i.test(M[1])){
+          tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+          return 'IE '+(tem[1] || '');
+      }
+      if(M[1]=== 'Chrome'){
+          tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+          if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+      }
+      M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+      if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+      return M.join(' ');
+    })();
+    this.versionOS=sayswho
   }
   showSuccess(m){
   	this.toastr.success(m,'Well Done',{positionClass:"toast-top-right"})
