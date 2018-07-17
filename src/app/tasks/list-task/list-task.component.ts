7d072fdbda2374cdf300abfe9b10a0fe98666bf1
bdangla-senorcoders <brian@senorcoders.com>
2018-07-17 14:53:52 -0600

    edit tasks

--- src/app/tasks/list-task/list-task.component.ts
+++ src/app/tasks/list-task/list-task.component.ts
@@ -5,6 +5,7 @@ import {FormBuilder, Validators, FormGroup} from "@angular/forms";
 import {TeamService} from '../../services/team.service';
 import {AuthenticationService} from '../../services/authentication.service';
 import { ToastrService } from 'ngx-toastr';
+import {INgxMyDpOptions} from 'ngx-mydatepicker';
 
 @Component({
   selector: 'app-list-task',
@@ -26,8 +27,23 @@ export class ListTaskComponent implements OnInit {
 	showPopup:boolean=false;
 	currentTask:any;
 	playerInfo:any;
+	players:any;
+	taskForm:FormGroup
+	seeTask:boolean=false
+	currentDate=new Date();
+	showPlayer:boolean=true;
+	taskID:any;
+	public myDatePickerOptions: INgxMyDpOptions = {
+        dateFormat: 'mm.dd.yyyy',
+        alignSelectorRight:true,
+        disableUntil:{
+        	year: this.currentDate.getFullYear(),
+		    month: this.currentDate.getMonth()+1,
+		    day: this.currentDate.getDate()-1,
+        }
+    };
   constructor(private team:TeamService, private auth:AuthenticationService, private toast:ToastrService,
-  	private pageTitle:PageTitleService) { }
+  	private pageTitle:PageTitleService, private fb:FormBuilder) { }
 
   ngOnInit() {
   	this.pageTitle.setTitle('List Tasks')
@@ -61,10 +77,67 @@ export class ListTaskComponent implements OnInit {
 		}
   	)
   }
+  getPlayerByTeam(id){
+	this.team.getData(`players?where={"team":"${id}"}`).subscribe(
+  		result=>{
+  			this.showPlayer=true;
+  			this.players=result;
+  		},
+  		e=>{
+  			console.log(e)
+	  	}
+  	)
+  }
   	smallDesc(str) {
     	return str.split(/\s+/).slice(0,10).join(" ");
 	}
+	saveTask(){
+		let dateTime=this.taskForm.get('dateTime').value;
+		let date=new Date(dateTime);
+		let formatDate;
+		if(dateTime!=''){
+			dateTime=dateTime['date']['year']+'-'+dateTime['date']['month']+'-'+dateTime['date']['day']+' '+this.taskForm.get('time').value
+			formatDate=date.toISOString()
+		}
+		let data={
+	  		name:this.taskForm.get('name').value,
+	  		text:this.taskForm.get('text').value,
+	  		dateTime:formatDate,
+	  		team:this.taskForm.get('team').value,
+	  		from:this.userData.id,
+	  		for:this.taskForm.get('for').value,
+	  		completad: this.taskForm.get('completad').value
+	  	}
+	  	this.team.editData('task/'+this.taskID,data).subscribe(
+	  		result=>{
+	  			this.toast.success("Task saved", "Well Done",{positionClass:"toast-top-right"} );
+	  			this.taskForm.reset();
+	  			this.showPopup=false;
+	  			this.getTaskByTeam(data.team)
+	  		},
+	  		e=>{
+	  			console.log(e)
+		      	this.toast.error("Something wrong happened. Please try again", "Error",{positionClass:"toast-top-right"} );
+	  		}
+	  	)
+	}
+	editTask(i){
+		this.taskID=this.tasks[i].id
+		this.showPopup=true
+		this.seeTask=false
+		this.getPlayerByTeam(this.tasks[i].team)
+		this.taskForm=this.fb.group({
+	  		name:[this.tasks[i].name, Validators.required],
+	  		text:[this.tasks[i].text, Validators.required],
+	  		team:[this.tasks[i].team],
+	  		dateTime:[''],
+	  		time:[''],
+	  		for:[this.tasks[i].for],
+	  		completad:[this.tasks[i].completad]
+		})
+	}
   showPopUp(i){
+  	this.seeTask=true
   	this.currentTask=this.tasks[i];
   	this.getUser(this.currentTask.for);
   }
