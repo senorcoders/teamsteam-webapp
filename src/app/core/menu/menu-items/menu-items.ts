e6ae48212077fc83684ec2cf4ddb4ebe4b5763ba
bdangla-senorcoders <brian@senorcoders.com>
2018-07-17 16:01:45 -0600

    remove player options

--- src/app/core/menu/menu-items/menu-items.ts
+++ src/app/core/menu/menu-items/menu-items.ts
@@ -57,47 +57,9 @@ const MENUITEMS = [
     
 
 ];
-/*menu for players*/
-const MENUPLAYER = [
-  {
-    state: 'horizontal',
-    name: 'HORIZONTAL LAYOUT',
-    type: 'link',
-    icon: 'icon-calendar icons'
-  },
-  {
-    state: 'dashboard',
-    name: 'DASHBOARD',
-    type: 'sub',
-    icon: 'icon-speedometer icons',
-    children: [
-      {state: 'dashboard-v1', name: 'DASHBOARD 1'},
-      {state: 'dashboard-v2', name: 'DASHBOARD 2'},
-    ]
-  },
-  {
-    state: 'teams',
-    name: 'TEAMS',
-    type: 'sub',
-    icon: 'icon-speedometer icons',
-    children: [
-      {state: 'list-team', name: 'List'},
-    ]
-  }
-
-];
 @Injectable()
 export class MenuItems {
   getAll(): Menu[] {
     return MENUITEMS;
-  }
-  /*getPlayerMenu():Menu[]{
-    return MENUPLAYER;
-  }*/
-  getTeamsMenu():Menu[]{
-    return MENUPLAYER;
-  }
-  add(menu: Menu) {
-    //MENUITEMS.push(menu);
   }
 }
