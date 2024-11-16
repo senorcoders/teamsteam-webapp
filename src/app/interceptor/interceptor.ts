c7f9c4177b78fd483c36a80c992b3a834976aa9e
Elvin Acuña <elvinwaz@outlook.com>
2024-11-15 21:15:44 -0600

    fix: Update deploy script

--- src/app/interceptor/interceptor.ts
+++ src/app/interceptor/interceptor.ts
@@ -1,42 +1,42 @@
-import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable'
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable'
 
 @Injectable()
 export class Interceptor implements HttpInterceptor {
 
-    public static url = "https://api.lockerroomapp.com"; // "http://192.168.1.8:8187";
+  public static url = 'https://api.lockerroomapp.com';
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-        let event: HttpRequest<any>;
-        let data = localStorage.getItem('sessionToken');
-        if (data) {
-            let json = JSON.parse(data);
-            let token = json['token'];
+    let event: HttpRequest<any>;
+    const data = localStorage.getItem('sessionToken');
+    if (data) {
+      const json = JSON.parse(data);
+      const token = json['token'];
 
-            event = req.clone({
-                url: Interceptor.url + req.url,
-                setHeaders: {
-                    'token': `${token}`
-                }
-            });
-        } else {
-            event = req.clone({
-                url: Interceptor.url + req.url
-            });
+      event = req.clone({
+        url: Interceptor.url + req.url,
+        setHeaders: {
+          'token': `${token}`
         }
-
-        return next.handle(event);
+      });
+    } else {
+      event = req.clone({
+        url: Interceptor.url + req.url
+      });
     }
 
-    public static transformUrl(url: string) {
-        let data = localStorage.getItem('sessionToken');
-        if (data) {
-            let json = JSON.parse(data);
-            let token = json['token'];
-            return Interceptor.url + url + `?token=${token}`;
-        }
-        return Interceptor.url + url;
+    return next.handle(event);
+  }
+
+  public static transformUrl(url: string) {
+    const data = localStorage.getItem('sessionToken');
+    if (data) {
+      const json = JSON.parse(data);
+      const token = json['token'];
+      return Interceptor.url + url + `?token=${token}`;
     }
+    return Interceptor.url + url;
+  }
 }
